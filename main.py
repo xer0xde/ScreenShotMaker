33c1235d21879bbc3fed95f92df2e6b33ca57bbd
xerox <77238267+xer0xde@users.noreply.github.com>
2024-01-26 12:39:26 +0100

    Update main.py
    Added Extension Option, better Quality + 16:9

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 import json
+import time
 
 # Pfade festlegen
 input_folder = "C:\\Users\\nicoh\\Documents\\ScreenShotMaker\\input"
 output_folder = "C:\\Users\\nicoh\\Documents\\ScreenShotMaker\\output"
+extension_path = "C:\\Pfad\\Zur\\Erweiterung" 
 
 os.makedirs(output_folder, exist_ok=True)
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("high-dpi-support=1")
-chrome_options.add_argument("force-device-scale-factor=1.0")
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-popup-blocking")
+chrome_options.add_argument("--disable-gpu")
+
+# Füge die Erweiterung hinzu
+chrome_options.add_argument(f"--load-extension={extension_path}")
+
 driver = webdriver.Chrome(options=chrome_options)
 
 for filename in os.listdir(input_folder):
@@ -25,22 +36,22 @@ for filename in os.listdir(input_folder):
         for item in data:
             title = item['title']
             url = item['website']
+            screenshot = item.get('screenshot', True)
 
-            if url is not None:
-                screenshot_filename = os.path.join(output_folder, f"{title}.png")
-
+            if url is not None and screenshot:
                 driver.get(url)
 
-                driver.implicitly_wait(5)
-
                 try:
-                    accept_button = driver.find_element("xpath", "//button[contains(text(), 'Accept')]")
+                    accept_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Accept')]")
                     accept_button.click()
                 except Exception as e:
                     print(f"Button nicht gefunden: {e}")
 
+                time.sleep(5)
 
-                driver.save_screenshot(screenshot_filename)
+                screenshot_filename = os.path.join(output_folder, f"{title}.png")
 
-# Schließe den Webdriver
+                driver.set_window_size(1920, 1080)  # 16:9-Aspektverhältnis
+                driver.save_screenshot(screenshot_filename, 'png')
+
 driver.quit()
